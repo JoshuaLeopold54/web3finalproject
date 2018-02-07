f3ba8eca58c93b5e3aba5ecb6135e1030953c7b9
josh <JoshuaLeopold54@gmail.com>
2018-02-07 15:35:03 -0600

    Added my actual main folder where server is, springboot essentials. (2/5/2018)

---
+++ main/sellyourstuff-api/src/main/java/io/Web3Project/TestAPI.java
@@ -0,0 +1,14 @@
+package io.Web3Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAPI.class, args);
+		
+	}
+
+}
